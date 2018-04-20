9434944b95610ed7475b070383dec46922e28e1c
Zofia Góralewska <zgoralewska@gmail.com>
2018-04-20 09:06:14 +0200

    Fix missing actionName (infinite requests in query hoc)

--- src/buildActionCreators.js
+++ src/buildActionCreators.js
@@ -4,6 +4,7 @@ export default function buildActionCreators(endpoints = {}) {
     return Object.keys(endpoints).reduce((acc, key) => {
         acc[key] = endpoints[key];
         acc[key].action = (...args) => callAction(endpoints[key], ...args);
+        acc[key].action.actionName = acc[key].actionName;
         return acc;
     }, {});
 }
